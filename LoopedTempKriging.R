2b1b1f602e371fbc7a1428a78accf3b9451be4ae
Shyam <thomas.shyam@gmail.com>
2019-10-10 14:47:16 -0400

    Update LoopedTempKriging.R

--- LoopedTempKriging.R
+++ LoopedTempKriging.R
@@ -14,6 +14,8 @@ library("automap", lib.loc="~/R/win-library/3.3")
 library(raster)
 library(automap)
 
+###################Creating a single interpolated surface for Ontario####
+
 setwd("C:/Users/thoma/Rprojects/OntarioClimateInterpolation")
 
 ### Start by reading and mapping all station for a given year with daily weather data (at least 334 days of data was set asthreshold)###
@@ -100,3 +102,131 @@ plot(OntLakes, border="white", add=TRUE)
 
 meantmp.lakes=extract(temp.raster.pred,OntLakes, fun = mean, na.rm = TRUE, sp=TRUE)
 head(meantmp.lakes)
+
+
+########################################################################################################################################
+########################################################################################################################################
+
+####Looping temperature datafiles to create multiple Kriged raasters######
+
+library(sp)
+library(rgdal)
+library(maptools)
+library(gstat)
+library(raster)
+library(automap)
+
+
+setwd("C:/Users/thoma/Rprojects/OntarioClimateInterpolation")
+
+ont.lamb <- "+proj=lcc +lat_1=60 +lat_2=46 +lat_0=44 +lon_0=-68.5 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs"
+
+ontborder = readShapePoly("1mprovnc.shp")
+proj4string(ontborder) = CRS(ont.lamb)
+class(ontborder)
+plot(ontborder)
+
+temp.files=list() #create an empty list
+listcsv = dir(path= "./TempFiles", pattern = "*.csv") #set the path and file extension
+listcsv # check!
+
+newnames=c("LakeName", "Lat", "Long", "TotalTemp", "SumofDays", "AverageTemp") #assigning a better set of names for columns
+
+## A simple for loop to read all the temperature csv files
+for (k in 1:length(listcsv)){
+temp.files[[k]] = read.csv(listcsv[k])
+colnames(temp.files[[k]])=newnames
+}
+class(temp.files)
+str(temp.files) #Check!
+
+
+png("Ont2008_2012StnsMeanTemp.png", width=5, height = 5, units = "in", res = 900)
+
+par(mfrow=c(2,3),mar=c(3,2.5,2,2.5))
+
+temp.labels=seq(2008,2012,1)
+rbPal = colorRampPalette(c('red','yellow','blue'))
+
+## Now another loop to assign the new names and more...
+
+for (i in 1:length(temp.files)){
+
+coordinates(temp.files[[i]])=cbind(temp.files[[i]]$Long,temp.files[[i]]$Lat)
+proj4string(temp.files[[i]]) = CRS(ont.lamb)
+plot(ontborder, main=temp.labels[i], axes=TRUE)
+temp.files[[i]]$Col= rbPal(12)[as.numeric(cut(temp.files[[i]]$AverageTemp, breaks=12))]
+plot(temp.files[[i]], add=TRUE, pch=16, cex=0.75, col=temp.files[[i]]$Col, title=temp.labels[i])
+}
+
+dev.off()
+
+
+###Now lets make an empty raster based on Ontario polygon and then grid it to make a kriging tempelate
+
+ont.ext=extent(ontborder)
+summary(ont.ext)
+
+ont.raster.null=raster(ont.ext, res=0.05)
+ont.raster.final=rasterize(ontborder,ont.raster.null, fun="first")
+summary(ont.raster.final)
+plot(ont.raster.final)
+
+ont.grid=as(ont.raster.final, "SpatialGridDataFrame")
+summary(ont.grid)
+plot(ont.grid)
+
+
+###For loop to reiterate the kriging process across the different data samples using the spatial grid of Ontario
+
+temp.autoKrig=list()
+temp.raster.pred=list()
+
+
+png("Ont2008_2012AnnMeanTemp_rasters.png", width=5, height = 5, units = "in", res = 900)
+par(mfrow=c(2,3),mar=c(3,2.5,2,2.5))
+
+      for (i in 1:length(temp.files)){
+          temp.autoKrig[[i]]=autoKrige(AverageTemp~1, temp.files[[i]], ont.grid)
+          #plot(temp.autoKrig[[i]])
+          temp.raster.pred[[i]]=raster(temp.autoKrig[[i]]$krige_output)
+          plot(temp.raster.pred[[i]], main=temp.labels[i])
+          }
+
+
+###Now another loop to capture mean lake temperature for each year
+
+OntLakes = readShapePoly("BsM_CleanedLakeBoundary.shp")
+proj4string(OntLakes) = CRS(ont.lamb)
+class(OntLakes)
+plot(OntLakes)
+
+meantmp.lakes=list()
+par(mfrow=c(2,3),mar=c(3,2.5,2,2.5))
+
+     for (i in 1:length(temp.raster.pred)){
+          #plot(temp.raster.pred[[i]])
+          #plot(studyarea, add=TRUE, border="black")
+          #plot(OntLakes, border="white", add=TRUE)
+          meantmp.lakes[[i]]=extract(temp.raster.pred[[i]],OntLakes, fun = mean, na.rm = TRUE, sp=TRUE)
+     }
+
+### final set of codes to extract the mean temp of all lakes for the 5 years
+
+laketemps = matrix(ncol=length(meantmp.lakes), nrow=length(meantmp.lakes[[1]]$var1.pred))
+for(i in 1:5){
+laketemps[,i] =meantmp.lakes[[i]]$var1.pred
+}
+laketemps.df=data.frame(laketemps)
+colnames(laketemps.df)=temp.labels
+class(laketemps.df)
+head(laketemps.df)
+
+meantmp.lakes.df=data.frame(meantmp.lakes[[1]][,c(1,6:10,14:21)])
+meantmp.lakes.final.df=cbind(meantmp.lakes.final.df,output.df)
+class(meantmp.lakes.final.df)
+head(meantmp.lakes.final.df)
+
+meantmp.lakes.final.df$MeanTemp = rowMeans(meantmp.lakes.final.df[,17:19])
+head(meantmp.lakes.final.df)
+write.csv(meantmp.lakes.final.df, "meantemp.ontlakes.2008_2012.csv")
